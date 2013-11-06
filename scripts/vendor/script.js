8b063cbe718aae1f2219d1bdfae124cf57ab7613
Mehdi Ghilaci <mehdi.ghilaci@gmail.com>
2013-11-06 18:13:54 +0100

    Carto WIP

--- scripts/vendor/script.js
+++ scripts/vendor/script.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
 
 	wH = $(window).height();
 	wW = $(window).width();
-	$('.card.active .inner').height(wH - 308 - 135);
+	$('.card.active .inner').height(wH - 308);
 
 	$(document).on('click','.photo-widget #picture-file', function(){
         $(this).siblings('input[type="file"]').click();
